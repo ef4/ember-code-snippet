5743b6a7a069759bf9a4590834c3b15c20a28979
simonihmig <simon.ihmig@kaliber5.de>
2019-05-25 18:58:31 +0200

    Add support for replacement of dynamic properties

--- addon/components/code-snippet.js
+++ addon/components/code-snippet.js
@@ -1,14 +1,24 @@
 import layout from '../templates/components/code-snippet';
 import Component from '@ember/component';
-import { computed } from '@ember/object';
+import { computed, get, defineProperty } from '@ember/object';
+import { htmlSafe } from '@ember/template';
 
 const Highlight = self.require('highlight.js');
+
+const quoteForLanguage = {
+  htmlbars: '"'
+};
 
 export default Component.extend({
   layout,
   tagName: 'pre',
   classNameBindings: ['language'],
   unindent: true,
+  quote: computed('language', {
+    get() {
+      return quoteForLanguage[this.get('language')] || '\'';
+    }
+  }),
 
   _unindent(src) {
     if (!this.get('unindent')) {
@@ -27,43 +37,102 @@ export default Component.extend({
     return src;
   },
 
-  source: computed('name', function(){
+  _dynamify(snippet) {
+    let dynamicProperties = this.get('_dynamicProperties');
+    Object.keys(dynamicProperties).forEach((property) => {
+      let propertyValue = get(dynamicProperties, property);
+      let type = typeof propertyValue;
+      let quote = this.get('quote');
+
+      let quotedValue = type === 'string' ? `${quote}${propertyValue}${quote}` : propertyValue;
+
+      switch (this.get('language')) {
+        case 'htmlbars':
+          snippet = snippet.replace(
+            new RegExp(`(<\\w*[^>]*\\s@[^=]+=){{${property}}}([^>]*>)`, 'g'),
+            type === 'string' ? `$1${quotedValue}$2` : `$1{{${propertyValue}}}$2`
+          );
+          snippet = snippet.replace(new RegExp(`{{${property}}}`, 'g'), propertyValue);
+          snippet = snippet.replace(new RegExp(`=${property}`, 'g'), `=${quotedValue}`);
+          break;
+        default:
+          snippet = snippet.replace(new RegExp(`{{${property}}}`, 'g'), quotedValue);
+      }
+    });
+
+    return snippet;
+  },
+
+  source: computed('name', '_dynamicProperties', function(){
     let snippet = this.get('name')
       .split('/')
-      .reduce((dir, name) => dir && dir[name], this.snippets);
+      .reduce((dir, name) => dir && dir[name], this.snippets) || '';
+
+    if (this.get('_dynamicProperties')) {
+      snippet = this._dynamify(snippet);
+    }
 
     return this._unindent(
-      (snippet || "")
+      snippet
         .replace(/^(\s*\n)*/, '')
         .replace(/\s*$/, '')
     );
   }),
 
-  didInsertElement(){
-    Highlight.highlightBlock(this.get('element'));
-  },
+  highlight: computed('source', 'language', function() {
+    let html = this.get('language')
+      ? Highlight.highlight(this.get('language'), this.get('source')).value
+      : Highlight.highlightAuto(this.get('source')).value;
 
-  language: computed('name', function(){
-    let m = /\.(\w+)$/i.exec(this.get('name'));
-    if (m) {
-      switch (m[1].toLowerCase()) {
-      case 'js':
-        return 'javascript';
-      case 'coffee':
-        return 'coffeescript';
-      case 'hbs':
-        return 'htmlbars';
-      case 'css':
-        return 'css';
-      case 'scss':
-        return 'scss';
-      case 'less':
-        return 'less';
-      case 'emblem':
-        return 'emblem';
-      case 'ts':
-        return 'typescript';
+    return htmlSafe(html);
+  }),
+
+  language: computed('name', {
+    get() {
+      let m = /\.(\w+)$/i.exec(this.get('name'));
+      if (m) {
+        switch (m[1].toLowerCase()) {
+          case 'js':
+            return 'javascript';
+          case 'coffee':
+            return 'coffeescript';
+          case 'hbs':
+            return 'htmlbars';
+          case 'css':
+            return 'css';
+          case 'scss':
+            return 'scss';
+          case 'less':
+            return 'less';
+          case 'emblem':
+            return 'emblem';
+          case 'ts':
+            return 'typescript';
+        }
       }
+    },
+    set(key, value) {
+      return value;
     }
-  })
+  }),
+
+  init() {
+    this._super(...arguments);
+
+    let dynamicProperties = this.get('dynamicProperties');
+    if (dynamicProperties) {
+      // define a property that gets updated correctly whenever any of the dynamic properties change
+      // by passing all its keys as dependent keys of the CP
+      defineProperty(
+        this,
+        '_dynamicProperties',
+        computed(
+          ...Object.keys(dynamicProperties).map(key => `dynamicProperties.${key}`),
+          function() {
+            return this.get('dynamicProperties');
+          }
+        )
+      );
+    }
+  }
 });
--- tests/integration/components/code-snippet-test.js
+++ tests/integration/components/code-snippet-test.js
@@ -17,11 +17,91 @@ module('Integration | Component | code-snippet', function(hooks) {
       {{code-snippet id="snippet" name="render-test.js"}}
     `);
 
-
-
     assert.dom('pre#snippet').exists();
     assert.dom('pre#snippet').hasText(
       'function sample(){\n  return 42;\n};'
+    );
+  });
+
+  test('it supports dynamic properties [js]', async function(assert) {
+    this.set('value', 42);
+    await render(hbs`
+      // BEGIN-SNIPPET dynamic-js-test
+      function sample(){
+        return {{value}};
+      };
+      // END-SNIPPET
+      
+      {{code-snippet id="snippet" name="dynamic-js-test.js" dynamicProperties=(hash value=value)}}
+    `);
+
+    assert.dom('pre#snippet').hasText(
+      'function sample(){\n  return 42;\n};'
+    );
+
+    this.set('value', "foo");
+    assert.dom('pre#snippet').hasText(
+      'function sample(){\n  return \'foo\';\n};'
+    );
+
+    this.set('value', true);
+    assert.dom('pre#snippet').hasText(
+      'function sample(){\n  return true;\n};'
+    );
+  });
+
+  test('it supports dynamic properties [hbs]', async function(assert) {
+    this.set('value', 42);
+    await render(hbs`
+      // BEGIN-SNIPPET dynamic-hbs-test
+      {{input value=value}}
+      Value: {{value}}
+      // END-SNIPPET
+      
+      {{code-snippet id="snippet" name="dynamic-hbs-test.js" language="htmlbars" dynamicProperties=(hash value=value)}}
+    `);
+
+    assert.dom('pre#snippet').hasText(
+      '{{input value=42}}\nValue: 42'
+    );
+
+    this.set('value', "foo");
+    assert.dom('pre#snippet').hasText(
+      '{{input value="foo"}}\nValue: foo'
+    );
+
+    this.set('value', true);
+    assert.dom('pre#snippet').hasText(
+      '{{input value=true}}\nValue: true'
+    );
+  });
+
+  test('it supports dynamic properties [hbs, angle-brackets]', async function(assert) {
+    this.owner.register('template:components/foo', hbs`{{yield}}`);
+    this.set('value', 42);
+    await render(hbs`
+      // BEGIN-SNIPPET dynamic-hbs-angle-test
+      <Foo
+        @value={{value}}
+      />
+      Value: {{value}}
+      // END-SNIPPET
+      
+      {{code-snippet id="snippet" name="dynamic-hbs-angle-test.js" language="htmlbars" dynamicProperties=(hash value=value)}}
+    `);
+
+    assert.dom('pre#snippet').hasText(
+      '<Foo @value={{42}} />\nValue: 42'
+    );
+
+    this.set('value', "foo");
+    assert.dom('pre#snippet').hasText(
+      '<Foo @value="foo" />\nValue: foo'
+    );
+
+    this.set('value', true);
+    assert.dom('pre#snippet').hasText(
+      '<Foo @value={{true}} />\nValue: true'
     );
   });
 });
