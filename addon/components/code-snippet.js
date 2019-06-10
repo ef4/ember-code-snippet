0374bcbeb9178d0925c81de231c904efb714ee88
simonihmig <simon.ihmig@kaliber5.de>
2019-06-10 20:49:42 +0200

    Removed code-snippet component

--- addon/components/code-snippet.js
+++
@@ -1,8 +0,0 @@
-import layout from '../templates/components/code-snippet';
-import Component from '@ember/component';
-
-export default Component.extend({
-  layout,
-  tagName: '',
-  unindent: true
-});
--- app/components/code-snippet.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-code-snippet/components/code-snippet';
