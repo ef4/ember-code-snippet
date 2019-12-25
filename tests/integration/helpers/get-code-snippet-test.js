1a0d7a0e9692bdec3fcb46d9b8700fbf59dfe3a4
ijlee2 <ijlee2@users.noreply.github.com>
2019-12-25 12:29:12 -0600

    Fixed a false positive test (due to a typo)

--- tests/integration/helpers/get-code-snippet-test.js
+++ tests/integration/helpers/get-code-snippet-test.js
@@ -15,18 +15,21 @@ module('Integration | Helper | get-code-snippet', function(hooks) {
       // END-SNIPPET
       
       {{#with (get-code-snippet "render-test.js") as |snippet|}}
-        <div id="source">{{snippet.source}}</div>
+        <pre id="source"><code>{{snippet.source}}</code></pre>
         <div id="language">{{snippet.language}}</div>
         <div id="extension">{{snippet.extension}}</div>
       {{/with}}
     `);
 
-    assert.dom('#source').hasText('function sample(){\n  return 42;\n};');
+    assert.strictEqual(
+      this.element.querySelector('#source').textContent,
+      'function sample(){\n  return 42;\n};'
+    );
     assert.dom('#language').hasText('javascript'); // language is determined by file extension, so JS in this case
     assert.dom('#extension').hasText('js');
   });
 
-  test('it returns snippet w/ intented source', async function(assert) {
+  test('it returns snippet w/ indented source', async function(assert) {
     await render(hbs`
       // BEGIN-SNIPPET render-test
       function sample(){
@@ -34,14 +37,17 @@ module('Integration | Helper | get-code-snippet', function(hooks) {
       };
       // END-SNIPPET
       
-      {{#with (get-code-snippet "render-test.js" unintent=false) as |snippet|}}
-        <div id="source">{{snippet.source}}</div>
+      {{#with (get-code-snippet "render-test.js" unindent=false) as |snippet|}}
+        <pre id="source"><code>{{snippet.source}}</code></pre>
         <div id="language">{{snippet.language}}</div>
         <div id="extension">{{snippet.extension}}</div>
       {{/with}}
     `);
 
-    assert.dom('#source').hasText('      function sample(){\n        return 42;\n      };');
+    assert.strictEqual(
+      this.element.querySelector('#source').textContent,
+      '      function sample(){\n        return 42;\n      };'
+    );
     assert.dom('#language').hasText('javascript'); // language is determined by file extension, so JS in this case
     assert.dom('#extension').hasText('js');
   });
